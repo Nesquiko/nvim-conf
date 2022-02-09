754977c6334fb058dce218e1438370b8f49df374
Nesquiko <lukascastven@gmail.com>
2022-02-09 21:38:41 +0100

    Add Project plugin.

--- lua/setup/plugins.lua
+++ lua/setup/plugins.lua
@@ -106,6 +106,9 @@ return packer.startup(function(use)
 	-- Java LSP JDTLS
 	use("mfussenegger/nvim-jdtls")
 
+	-- Project
+	use("ahmedkhalf/project.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
