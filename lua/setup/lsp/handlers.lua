74f0d968672ab0e8c5f2ed5ea20000771f43ada9
Nesquiko <lukascastven@gmail.com>
2022-02-11 16:27:15 +0100

    Improve format on save.

--- lua/setup/lsp/handlers.lua
+++ lua/setup/lsp/handlers.lua
@@ -89,14 +89,12 @@ M.on_attach = function(client, bufnr)
 		client.resolved_capabilities.document_formatting = false
 	end
 
-	if client.resolved_capabilities.document_formatting then
-		vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
-            ]])
-	end
+	vim.cmd([[
+	       augroup LspFormatting
+	           autocmd! * <buffer>
+	           autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+	       augroup END
+	       ]])
 
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
@@ -111,5 +109,15 @@ if not status_ok then
 end
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+
+function M.enable_format_on_save()
+	vim.cmd([[
+    augroup format_on_save
+      autocmd! 
+      autocmd BufWritePre * lua vim.lsp.buf.formatting()
+    augroup end
+  ]])
+	vim.notify("Enabled format on save")
+end
 
 return M
