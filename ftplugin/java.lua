9a924a9d01184d46b6eaff9c3a6f5e1486fa15e0
Nesquiko <lukascastven@gmail.com>
2022-02-15 09:42:31 +0100

    Remove unnecessary comments in jdtls setup.

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -74,6 +74,14 @@ local config = {
 	-- for a list of options
 	settings = {
 		extendedClientCapabilities = extendedClientCapabilities,
+		java = {
+			autobuild = {
+				enabled = false,
+			},
+			project = {
+				outputPath = "abcd",
+			},
+		},
 		sources = {
 			organizeImports = {
 				starThreshold = 9999,
@@ -102,34 +110,3 @@ local config = {
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
 require("jdtls").start_or_attach(config)
-
-local opts = { noremap = true, silent = true }
--- vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
--- vim.api.nvim_set_keymap(
--- 	"n",
--- 	"<leader>wl",
--- 	"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
--- 	opts
--- )
--- vim.api.nvim_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
--- vim.api.nvim_set_keymap(
--- 	"n",
--- 	"gl",
--- 	'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
--- 	opts
--- )
--- vim.api.nvim_set_keymap("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
--- vim.api.nvim_set_keymap("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
--- vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
--- vim.api.nvim_set_keymap("n", "<leader>ll", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
---
--- -- more on https://github.com/mfussenegger/nvim-jdtls#usage
-vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
