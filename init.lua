74d63afc221c4d5cfc4e55ac93033460fe86d8a1
Nesquiko <lukascastven@gmail.com>
2022-05-28 22:20:47 +0200

    Pcall keymaps.lua and log if error.

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
-require("setup.keymaps")
+-- init.lua
+-- Main config file for nvim. Every initialization is called with in a pcall,
+-- so if one plugin fails, nvim is still loaded. When an error occurs, a log
+-- file is created in ./log dir.
+
+require("core.log")
+local data = vim.fn.stdpath("data")
+local file_name = data .. "/log/" .. Get_date_time() .. ".log"
+local file = io.open(file_name, "a")
+local wasError = false
+
+local ok, err = pcall(require, "setup.keymaps")
+if not ok and file then
+	wasError = true
+	file:write("setup.keymaps:\n" .. err .. "\n")
+end
+
 require("setup.plugins")
 require("setup.options")
 require("setup.cmp")
@@ -20,3 +36,10 @@ require("setup.alpha")
 require("setup.surround")
 require("setup.yabs")
 require("setup.debugger")
+
+file:close()
+if wasError then
+	vim.notify("An error occured, see logs for error info")
+else
+	os.remove(file_name)
+end
