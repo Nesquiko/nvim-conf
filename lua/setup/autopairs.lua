a0655acc7e7dfff07e2944fc7cfb768b84f6cbd9
Nesquiko <lukascastven@gmail.com>
2022-06-07 20:12:22 +0200

    Fix mistake in autopairs.
    Signed-off-by: Nesquiko <lukascastven@gmail.com>

--- lua/setup/autopairs.lua
+++ lua/setup/autopairs.lua
@@ -25,5 +25,5 @@ autopairs.setup({
 })
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = pcall(require, "cmp")
+local cmp = require("cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
