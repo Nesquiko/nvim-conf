d81a8e9a5979708a3c077198aba34250a3def6fd
Častven, Lukáš <lukas.castven@eea.sk>
2022-06-17 20:03:13 +0200

    Document null ls and change preferences.
    Signed-off-by: Častven, Lukáš <lukas.castven@eea.sk>

--- lua/setup/lsp/null-ls.lua
+++ lua/setup/lsp/null-ls.lua
@@ -1,7 +1,18 @@
-local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-if not null_ls_status_ok then
-	return
-end
+-- null-ls.lua
+-- Config file for jose-elias-alvarez/null-ls.nvim
+--
+-- PYTHON:
+-- 	- black for formatting
+-- 	- flake8 for diagnostics
+--
+-- GOLANG:
+-- 	- golines for formatting (also it runs goimports)
+-- 	- golangci_lint for formatting
+--
+-- LUA:
+-- 	- stylua for formatting
+
+local null_ls = require("null-ls")
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
@@ -10,12 +21,12 @@ local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
+	diagnostics_format = "[#{c}] #{m} (#{s})",
 	sources = {
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		formatting.goimports,
-		formatting.gofmt,
 		formatting.golines,
+
 		diagnostics.flake8.with({ extra_args = { "--max-line-length", "88" } }),
 		diagnostics.golangci_lint,
 	},
