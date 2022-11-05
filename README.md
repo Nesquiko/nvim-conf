57fbc6543787ba5c7d5e6b436482d29e163e36e5
Nesquiko <lukas.castven@gmail.com>
2022-11-05 20:59:04 +0100

    use map in dap.init

--- README.md
+++ README.md
@@ -5,9 +5,8 @@ My personal neovim config.
 ## TODO
 
 1. winbar with filename? (remove lualine)
-2. lualine add warnings and filetype
-3. Make a Telescope picker from commads (https://github.com/nvim-telescope/telescope-dap.nvim)
-4. automatic lsp installation through lsp-installer
+2. Make a Telescope picker from commads (https://github.com/nvim-telescope/telescope-dap.nvim)
+3. automatic lsp installation through lsp-installer/mason
 
 ## Plugins to look at
 
--- lua/plugin/config/debugger/init.lua
+++ lua/plugin/config/debugger/init.lua
@@ -8,20 +8,20 @@ require("dap")
 vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapStopped", { text = "", texthl = "", linehl = "", numhl = "" })
 
-ADD_KEYMAP("n", "<C-b>", ":lua require('dap').toggle_breakpoint()<CR>")
-ADD_KEYMAP(
+Map("n", "<C-b>", ":lua require('dap').toggle_breakpoint()<CR>")
+Map(
 	"n",
 	"<leader>b",
 	":lua require('dap').toggle_breakpoint(vim.fn.input('Breakpoint Condition: '), nil, nil, true)<CR>"
 )
-ADD_KEYMAP("n", "<F5>", ":lua require('dap').continue()<CR>")
-ADD_KEYMAP("n", "<F29>", ":lua require('dap').terminate()<CR>")
-ADD_KEYMAP("n", "<F6>", ":lua require('dap').step_into()<CR>")
-ADD_KEYMAP("n", "<F30>", ":lua require('dap').step_out()<CR>")
-ADD_KEYMAP("n", "<F7>", ":lua require('dap').step_over()<CR>")
+Map("n", "<F5>", ":lua require('dap').continue()<CR>")
+Map("n", "<F29>", ":lua require('dap').terminate()<CR>")
+Map("n", "<F6>", ":lua require('dap').step_into()<CR>")
+Map("n", "<F30>", ":lua require('dap').step_out()<CR>")
+Map("n", "<F7>", ":lua require('dap').step_over()<CR>")
 
-ADD_KEYMAP("n", "<F4>", ":lua require('dap').repl.open()<CR>")
-ADD_KEYMAP("n", "<F28>", ":lua require('dap').repl.close()<CR>")
+Map("n", "<F4>", ":lua require('dap').repl.open()<CR>")
+Map("n", "<F28>", ":lua require('dap').repl.close()<CR>")
 
 require("plugin.config.debugger.dap-events")
 require("plugin.config.debugger.nvim-dap-go")
