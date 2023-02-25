68d4e072e38bc0b24a51bcb900520846c77bfce0
Nesquiko <lukas.castven@gmail.com>
2023-02-25 11:50:15 +0100

    fix typo

--- lua/plugin/config/debugger/nvim-dap-go.lua
+++ lua/plugin/config/debugger/nvim-dap-go.lua
@@ -1,6 +1,6 @@
 -- nvim-dap-go.lua
 --
--- Config file for setting up nvim-dap debugget adapter for Golang.
+-- Config file for setting up nvim-dap debug adapter for Golang.
 
 local dap_go = require("dap-go")
 dap_go.setup()
