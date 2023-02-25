bbb99c8b1acb2244df997a2e75e4f434871d7fcd
Nesquiko <lukas.castven@gmail.com>
2023-02-25 11:50:31 +0100

    add tailwindcss lsp

--- lua/plugin/config/lsp/mason.lua
+++ lua/plugin/config/lsp/mason.lua
@@ -31,6 +31,7 @@ lspconfig.rust_analyzer.setup(opts)
 lspconfig.marksman.setup(opts)
 lspconfig.solidity.setup(opts)
 lspconfig.tsserver.setup(opts)
+lspconfig.tailwindcss.setup(opts)
 lspconfig.bashls.setup(opts)
 
 local lua_ls_opts = require("plugin.config.lsp.settings.sumneko_lua")
