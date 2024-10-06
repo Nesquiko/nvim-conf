babb2bce1531d5a7485674e509878c25df8fbce5
Nesquiko <lukas.castven@gmail.com>
2024-10-06 11:02:02 +0200

    confirm completion with enter

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -18,7 +18,7 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<C-y>"] = cmp.mapping(cmp.mapping.confirm({
+		["<CR>"] = cmp.mapping(cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Insert,
 			select = true,
 		}, { "i", "c" })),
