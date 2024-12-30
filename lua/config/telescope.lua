e242d057ebb892325eb4d42f0158e7fdf77527b7
Nesquiko <lukas.castven@gmail.com>
2024-12-30 22:23:21 +0100

    telescope show hidden files and dont show big files

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,4 +1,30 @@
 local builtin = require("telescope.builtin")
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+		preview = {
+			filesize_limit = 0.1, -- MB
+		},
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
