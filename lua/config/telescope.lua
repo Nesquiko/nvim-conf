d2bc995f9f6b826b81ac497c2c6a0e4a33b08eb9
Nesquiko <lukas.castven@gmail.com>
2025-01-12 19:21:21 +0100

    telescope ivy for live grep, help tags picker, nvim config picker

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -23,13 +23,20 @@ telescope.setup({
 			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
 			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 		},
+		live_grep = {
+			theme = "ivy",
+		},
 	},
 })
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>vc", function()
+	builtin.find_files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "Telescope find files in nvim config" })
 vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Telescope live grep" })
 
+vim.keymap.set("n", "<leader>ht", builtin.help_tags, { desc = "Telescope man pages" })
 vim.keymap.set("n", "<leader>mp", builtin.man_pages, { desc = "Telescope man pages" })
 vim.keymap.set("n", "<leader>cs", builtin.colorscheme, { desc = "Telescope color scheme picker" })
 vim.keymap.set("n", "<leader>ss", builtin.spell_suggest, { desc = "Telescope spelling suggestions" })
