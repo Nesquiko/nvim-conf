dc50fa37ae28fcd37b68821547eb08a893619f08
Nesquiko <lukas.castven@gmail.com>
2025-05-22 14:22:48 +0200

    execute current query in dadbod

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -10,8 +10,17 @@ return {
 		"DBUIAddConnection",
 		"DBUIFindBuffer",
 	},
+	ft = "sql",
 	init = function()
 		vim.g.db_ui_use_nerd_fonts = 1
 		vim.g.db_ui_win_position = "right"
+		vim.g.db_ui_execute_on_save = 0
+
+		vim.keymap.set(
+			"n",
+			"<leader>rq",
+			":normal vip<CR><PLUG>(DBUI_ExecuteQuery)",
+			{ desc = "run query under cursor" }
+		)
 	end,
 }
